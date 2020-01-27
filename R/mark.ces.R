a19766b3756218ebf0c6492984cc10d13ec3e2d6
Rob Rob <rob.robinson@bto.org>
2020-01-27 10:23:45 +0000

    increase reliability of ch extraction

--- R/mark.ces.R
+++ R/mark.ces.R
@@ -1,9 +1,9 @@
 mark.ces <-
-function(x, exclude=NULL, type='+', trend=0, compare=0, cleanup=FALSE){
+function(cesobj, exclude=NULL, type='+', trend=0, compare=0, cleanup=FALSE){
   
-  suppressMessages(require(RMark, quietly=TRUE))
+  requireNamespace('RMark', quietly=TRUE)
   
-  if( class(x)[1]!="ces" | class(x)[2]!='ch' )
+  if( class(cesobj)[1]!="ces" | class(cesobj)[2]!='ch' )
     stop('use extract.ch() to create a Mark data list')
   
   if( type != '+' & type != ':' )
@@ -19,34 +19,36 @@ function(x, exclude=NULL, type='+', trend=0, compare=0, cleanup=FALSE){
     setwd('markfiles')
   }  
   
+  chdata <- cesobj$chdata
+  
   if( !is.null(exclude) ){
     # first convert site IDs to site number
-    excl.rows <- which(as.character(x$sitenames) %in% as.character(exclude))
-    include.rows <- !(x$mr_data$site %in% excl.rows)
-    x$mr_data <- x$mr_data[include.rows, ]
-    x$mr_data$site <- droplevels(x$mr_data$site)
+    excl.rows <- which(as.character(cesobj$sitename) %in% as.character(exclude))
+    include.rows <- !(chdata$site %in% excl.rows)
+    chdata <- chdata[include.rows, ]
+    chdata$sitename <- droplevels(chdata$sitename)
   }
-  if( length(levels(x$mr_data$site)) == 1 ) # check if only 1 site
-    groups <- x$group$name
+  if( length(levels(chdata$sitename)) == 1 ) # check if only 1 site
+    groups <- cesobj$group$name
   else 
-    groups <- c("site", x$group$name)
+    groups <- c("sitename", cesobj$group$name)
 
-  groups <- groups[!is.na(groups)]   # x$group will be NA if group isn't set
+  groups <- groups[!is.na(groups)]   # cesobj$group will be NA if group isn't set
   
   nosite <- FALSE
   if( length(groups) == 0 ){
-    x.pd <- RMark::process.data(x$mr_data, begin.time=x$begin.time)
+    x.pd <- RMark::process.data(chdata, begin.time=cesobj$begin.time)
     nosite <- TRUE # only 1 site so cant fit this factor
   } else 
-    x.pd <- RMark::process.data(x$mr_data, begin.time=x$begin.time, groups=groups)
+    x.pd <- RMark::process.data(chdata, begin.time=cesobj$begin.time, groups=groups)
   
   ddl <- RMark::make.design.data(x.pd, parameters=list(Phi=list(age.bins=c(0,1,100)),
                                              p=list(age.bins=c(0,1,100))))
   ddl$Phi$time_var <- as.factor(ifelse(ddl$Phi$Cohort==ddl$Phi$Time, 1, ddl$Phi$time)) # Separate initial capture period
-  if( length(ddl$p$site) == 0 )
+  if( length(ddl$p$sitename) == 0 )
     ddl$p$time_var <- as.factor(ifelse(ddl$p$Cohort==ddl$p$Time, 0, 1)+1)
   else 
-    ddl$p$time_var <- as.factor(ifelse(ddl$p$Cohort==ddl$p$Time, 0, ddl$p$site)+1)
+    ddl$p$time_var <- as.factor(ifelse(ddl$p$Cohort==ddl$p$Time, 0, ddl$p$sitename)+1)
 
   
 # ddl$Phi$initial <- ifelse(ddl$Phi$Cohort==ddl$Phi$Time, 1, 0)   # create initial capture period
@@ -76,35 +78,35 @@ function(x, exclude=NULL, type='+', trend=0, compare=0, cleanup=FALSE){
   if( trend > 0 ){
     trend <- floor(trend) # just in case
     # work out Time period to start the trend
-    nyrs <- x$years - ifelse(trend > x$years, x$years, trend) + 1 
+    nyrs <- cesobj$years - ifelse(trend > cesobj$years, cesobj$years, trend) + 1 
     ddl$Phi$Tind <- ifelse(ddl$Phi$Time >= nyrs, 1, 0) # years with a trend 
     ddl$Phi$tind <- 1 - ddl$Phi$Tind # years before trend
     ddl$Phi$tind[ddl$Phi$time_var==1] <- 1 # make sure the transient year is picked up
     ddl$Phi$Tind[ddl$Phi$tind==1] <- 0 # but don't fit separate transient during compare period
-    if( is.na(x$group$name) )
+    if( is.na(cesobj$group$name) )
       phi.ces <- list(formula = as.formula('~tind:time_var+Tind:Time'))
     else
-      phi.ces <- list(formula = as.formula(paste0('~', x$group$name, '+tind:time_var+', x$group$name, ':Tind:Time')))
+      phi.ces <- list(formula = as.formula(paste0('~', cesobj$group$name, '+tind:time_var+', cesobj$group$name, ':Tind:Time')))
     model.name <- 'trend'
-    model.yrs <- ifelse(trend > x$years, x$years, trend)
+    model.yrs <- ifelse(trend > cesobj$years, cesobj$years, trend)
   } else if( compare > 0 ){
     compare <- floor(compare) # just in case
-    nyrs <- x$years - ifelse(compare > x$years, x$years, compare) + 1 
+    nyrs <- cesobj$years - ifelse(compare > cesobj$years, cesobj$years, compare) + 1 
     ddl$Phi$Cind <- ifelse(ddl$Phi$Time >= nyrs, 1, 0) # years within compare period 
     ddl$Phi$tind <- 1 - ddl$Phi$Cind # years before compare
     ddl$Phi$tind[ddl$Phi$time_var==1] <- 1 # make sure the transient year is picked up
     ddl$Phi$Cind[ddl$Phi$tind==1] <- 0 # but don't fit separate transient during compare period
-    if( is.na(x$group$name) )
+    if( is.na(cesobj$group$name) )
       phi.ces <- list(formula = as.formula('~tind:time_var+Cind'))
     else
-      phi.ces <- list(formula = as.formula(paste0('~', x$group$name, '+tind:time_var+', x$group$name, ':Cind')))
+      phi.ces <- list(formula = as.formula(paste0('~', cesobj$group$name, '+tind:time_var+', cesobj$group$name, ':Cind')))
     model.name <- 'constant'
-    model.yrs <- ifelse(compare > x$years, x$years, compare)
+    model.yrs <- ifelse(compare > cesobj$years, cesobj$years, compare)
   } else {
-    if( is.na(x$group$name) )
+    if( is.na(cesobj$group$name) )
       phi.ces <- list(formula = as.formula('~time_var'))
     else
-      phi.ces <- list(formula = as.formula(paste('~', paste('time_var', x$group$name, sep=type))))    
+      phi.ces <- list(formula = as.formula(paste('~', paste('time_var', cesobj$group$name, sep=type))))    
     model.name <- 'annual'
     model.yrs <- 0
   }
@@ -112,7 +114,7 @@ function(x, exclude=NULL, type='+', trend=0, compare=0, cleanup=FALSE){
   if( nosite )
     p.ces <- list(formula=~age)
   else 
-    p.ces <- list(formula=~site+age)
+    p.ces <- list(formula=~sitename+age)
   
   ## Now run the MARK models
   model.ces <- RMark::make.mark.model(x.pd, ddl, parameters=list(Phi=phi.ces,p=p.ces))
@@ -125,21 +127,21 @@ function(x, exclude=NULL, type='+', trend=0, compare=0, cleanup=FALSE){
   phi_indices <- grep('Phi', rownames(model$results$real))
   phi_res <- model$results$real[phi_indices, ]
   
-  rrows <- grep("a0",rownames(phi_res))
-  r_res <- phi_res[rrows,1:4]
-  s_res <- phi_res[-(rrows),1:4]
+  rrows <- grep("a0", rownames(phi_res))
+  r_res <- phi_res[rrows, 1:4]
+  s_res <- phi_res[-(rrows), 1:4]
   
-  if( !is.na(x$group$name) ){
-    r_res$group <- x$group$levels
-    r_res <- r_res[ ,c(5,1:4)]
-    s_res$group <- rep(x$group$levels,each=x$years)
-    s_res$years <- rep(c(x$begin.time:(x$begin.time+x$years-1)),length(x$group$levels))  
-    s_res <- s_res[ ,c(5,6,1:4)]
+  if( !is.na(cesobj$group$name) ){
+    r_res$group <- cesobj$group$levels
+    r_res <- r_res[ , c(5,1:4)]
+    s_res$group <- rep(cesobj$group$levels, each=cesobj$years)
+    s_res$years <- rep(c(cesobj$begin.time:(cesobj$begin.time+cesobj$years-1)),length(cesobj$group$levels))  
+    s_res <- s_res[ , c(5,6,1:4)]
   } else {
     if( compare > 0 ) # replicate the last row to generate enough annual estimates
       s_res <- s_res[c(1:nrow(s_res), rep(nrow(s_res), model.yrs-1)), ]
-    s_res$years <- c(x$begin.time:(x$begin.time+x$years-1))  
-    s_res <- s_res[ ,c(5,1:4)]
+    s_res$years <- c(cesobj$begin.time:(cesobj$begin.time+cesobj$years-1))  
+    s_res <- s_res[ , c(5,1:4)]
   }
   rownames(r_res) <- NULL
   rownames(s_res) <- NULL
@@ -151,14 +153,14 @@ function(x, exclude=NULL, type='+', trend=0, compare=0, cleanup=FALSE){
   p_res <- p0_res[p_indices,1:4]
   sites <- as.character(t(as.data.frame(strsplit(rownames(p_res),' '))[2,]))
   sites <- as.numeric(gsub("[[:alpha:]]", '', sites)) # remove any alpha chars, hopefully groupnames don't contain numbers!
-  p_res$sitename <- x$sitenames[sites]
-  p_res <- p_res[ ,c(5,1:4)]
+  p_res$sitename <- sites
+  p_res <- p_res[ , c(5,1:4)]
   rownames(p_res) <- NULL
   # the site specific first-year recaptures
   p1_indices <- grep('a2',rownames(p0_res))
-  p1_res <- p0_res[p1_indices,1:4]
-  p1_res$sitename <- x$sitenames[sites]
-  p1_res <- p1_res[ ,c(5,1:4)]
+  p1_res <- p0_res[p1_indices, (1:4)]
+  p1_res$sitename <- sites
+  p1_res <- p1_res[ , c(5,1:4)]
   rownames(p1_res) <- NULL
   
   setwd(oldwd)
@@ -170,9 +172,10 @@ function(x, exclude=NULL, type='+', trend=0, compare=0, cleanup=FALSE){
        survival=s_res,
        recapture=p_res,
        recap1=p1_res,
-       group=x$group,            
-       spp.name=x$spp.name)
+       group=cesobj$group,            
+       spp.name=cesobj$spp.name)
   class(results) <- c('ces', 'markfit')
-  results
+  
+  return(results)
 }
 

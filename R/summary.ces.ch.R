a0baa904cad8c854d533b8d9ae7f62560c58aa7d
Rob Rob <rob.robinson@bto.org>
2020-01-27 10:26:57 +0000

    streamline summary methods

--- R/summary.ces.ch.R
+++ R/summary.ces.ch.R
@@ -1,26 +1,28 @@
 summary.ces.ch <-
-function(x){
+function(cesobj){
   
-  nind <- length(x$mr_data[ , 1])
-  nocc <- nchar(x$mr_data$ch[1]) - 1 # because added an initial time period for residency
+  chdata <- cesobj$chdata
+  
+  nind <- length(chdata[ , 1])
+  nocc <- nchar(chdata$ch[1]) - 1 # because added an initial time period for residency
   
   # species and years
-  chmat <- RMark::splitCH(x$mr_data$ch)
+  chmat <- RMark::splitCH(chdata$ch)
   
   fy <- function(x){ min(which(x == "1")) }
-  first.year <- apply(chmat, 1, fy) + x$begin.time-1
-    
-  smessage <- paste(nind, "adults of", x$spp.name,
+  first.year <- apply(chmat, 1, fy) + cesobj$begin.time-1
+  
+  smessage <- paste(nind, "adults of", cesobj$spp.name,
                     "captured on", nocc, "occasions,",
-                    "at", length(unique(x$mr_data$site)), "sites", 
-                    "starting in", x$begin.time, "\n\n", sep=" ")
+                    "at", length(unique(chdata$site)), "sites", 
+                    "starting in", cesobj$begin.time, "\n\n", sep=" ")
   cat(smessage)
-
+  
   # number of birds by year
-  if ( is.na(x$group$name) )
+  if ( is.na(cesobj$group$name) )
     nbirds <- table(first.year, dnn=NULL)
   else 
-    nbirds <- t(table(first.year, by=x$mr_data[ ,c(x$group$name)], dnn=NULL))
+    nbirds <- t(table(first.year, by=chdata[ , c(cesobj$group$name)], dnn=NULL))
   cat('Number of birds first ringed in each year\n')
   print(nbirds)
   cat('\n')
@@ -33,10 +35,10 @@ function(x){
   cat('\n')
   
   # number of sites
-  if ( is.na(x$group$name) )
-    sites <- table(x$mr_data$site, dnn=NULL)
+  if ( is.na(cesobj$group$name) )
+    sites <- table(chdata$site, dnn=NULL)
   else
-    sites <- t(table(x$mr_data$site, by=x$mr_data[ ,c(x$group$name)], dnn=NULL))
+    sites <- t(table(chdata$site, by=chdata[ , c(cesobj$group$name)], dnn=NULL))
   cat('Number of individuals captured at each site\n')
   print(sites)  
   cat('\n')
@@ -44,5 +46,5 @@ function(x){
   invisible(list(nbirds = nbirds, 
                  nretraps = nretraps,
                  sites = sites))
-    
+  
 }
--- R/summary.ces.glmfit.R
+++ R/summary.ces.glmfit.R
@@ -1,24 +1,24 @@
 summary.ces.glmfit <-
-function(x, ads=FALSE, jvs=FALSE, prod=FALSE){
+function(cesobj, ads=FALSE, jvs=FALSE, prod=FALSE){
 
-  if ( !class(x)[1] == 'ces' | !class(x)[2] == 'glmfit' )
+  if ( !class(cesobj)[1] == 'ces' | !class(cesobj)[2] == 'glmfit' )
     stop("No information to summarise\n")
   if ( ads == FALSE & jvs==FALSE & prod == FALSE ){
     ads <- jvs <- prod <- TRUE
   }
-
+  
   res <- list()
-  if ( ads==TRUE & length(x$ad.results) > 0 ){ 
-    cat(sprintf('Adult Abundance (%s)\n',x$spp.name))
-    res$adults <- prsumglm(x$ad.results, x$model.type)
+  if ( ads==TRUE & length(cesobj$ad.results) > 0 ){ 
+    cat(sprintf('Adult Abundance (%s)\n',cesobj$spp.name))
+    res$adults <- prsumglm(cesobj$ad.results, cesobj$model.type)
   }
-  if ( jvs==TRUE & length(x$jv.results) > 0 ){    
-    cat(sprintf('Juvenile Abundance (%s)\n',x$spp.name))
-    res$juveniles <- prsumglm(x$jv.results, x$model.type)
+  if ( jvs==TRUE & length(cesobj$jv.results) > 0 ){    
+    cat(sprintf('Juvenile Abundance (%s)\n',cesobj$spp.name))
+    res$juveniles <- prsumglm(cesobj$jv.results, cesobj$model.type)
   }
-  if ( prod==TRUE & length(x$ad.results) & length(x$jv.results) > 0 ){ 
-    cat(sprintf('Productivity (%s)\n',x$spp.name))
-    res$productvity <- prsumglm(x$pr.results, x$model.type)
+  if ( prod==TRUE & length(cesobj$ad.results) & length(cesobj$jv.results) > 0 ){ 
+    cat(sprintf('Productivity (%s)\n',cesobj$spp.name))
+    res$productvity <- prsumglm(cesobj$pr.results, cesobj$model.type)
   }
   
   invisible(res)

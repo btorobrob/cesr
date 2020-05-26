f608409ba6df03e69c3616cd9c6082547c68820f
Rob Rob <rob.robinson@bto.org>
2020-05-26 22:29:31 +0100

    improve data filtering and add summary msg

--- R/extract.data.R
+++ R/extract.data.R
@@ -1,25 +1,29 @@
 extract.data <-
-function(data, species=0, plots=NULL){
+function(cesdata, species=0, plots=NULL, min.yrs=1){
 
-  if( !(class(data)[1] == 'ces' & class(data)[2] == 'data')  )
+  if( !(class(cesdata)[1] == 'ces' & class(cesdata)[2] == 'data') )
     stop("no ces data\n")
   
   if( is.null(plots) )
     stop("no plot data, use extract.coverage() first")
 
-  if( species == 0 ) {
+  if( species == 0 | !is.numeric(species) ) {
     species <- unique(data$species)
-    if( length(species) > 1 ) 
-      stop("please supply a Euring species code")
+    if( length(species) > 1 | !is.numeric(species) ) 
+      stop("please supply a valid Euring species code")
   }
- 
-  selspp <- as.numeric(species[1])
+  selspp <- as.numeric(species) # data.table seems to get confused if this is the same as a colname
   
   # temporarily convert for speed
-  data <- data.table::data.table(data, key='species')
-  data <- as.data.frame(data[species==selspp, .(site, visit, year, ring, age)])
+  data <- data.table::data.table(cesdata, key='species')
+  data <- data[(species==selspp & visit <= plots$all.visits), .(site, visit, year, ring, age)]
+  if( min.yrs > 1 ){
+    data <- merge(data, plots$sites[ , c('site', 'nyears')], by='site', all.x=TRUE)
+    data <- data[nyears >= min.yrs, ]
+  }
+  data <- as.data.frame(data)
   class(data) <- c("ces", "data", "data.frame") # expected by extract.counts()
-  
+
   if( nrow(data) == 0 )
     stop('no data for species ', species, '\n')
 
@@ -28,7 +32,11 @@ function(data, species=0, plots=NULL){
 
   lang <- getOption('ceslang')
   spp.name <- as.character(cesnames[cesnames$spp==species, which(colnames(cesnames)==lang) ])
-
+  
+  njuvs <- ifelse(is.null(jvcounts), 0, sum(jvcounts$totcaps))
+  nads <- ifelse(is.null(adcounts), 0, sum(adcounts$totcaps))
+  cat(paste("Extracted", njuvs, "juvenile and", nads, "adult captures\n"))
+  
   result <- list(ad.data = adcounts, 
                  jv.data = jvcounts, 
                  spp = species,

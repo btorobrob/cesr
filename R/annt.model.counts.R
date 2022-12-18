9a7b32d8f9ae1b72567bac7333e301b589739cba
Rob Rob <rob.robinson@bto.org>
2022-12-18 23:01:12 +0000

    bug fix and centre trendline

--- R/annt.model.counts.R
+++ R/annt.model.counts.R
@@ -18,9 +18,9 @@ function(x, year=-1, trend=100, offset=TRUE, cl=0.95){
     yearf <- ifelse ( x$year > ybreak, 0, x$year )
     yeart <- ifelse ( x$year > ybreak, x$year-ybreak, 0 )
     if( length(table(x$site)) > 1 )
-      x.lm <- glm(totcaps ~ as.factor(site) + as.factor(yearf) + yeart, family=quasipoisson, offset=offset, data=x)
+      x.lm <- glm(totcaps ~ as.factor(site) + as.factor(yearf) + yeart, family="quasipoisson", offset=offset, data=x)
     else
-      x.lm <- glm(totcaps ~ as.factor(yearf) + yeart, family=quasipoisson, offset=offset, data=x)
+      x.lm <- glm(totcaps ~ as.factor(yearf) + yeart, family="quasipoisson", offset=offset, data=x)
     
     yearf1 <- c(min(yearf[yearf>0]):max(yearf), rep(0, trend))
     yeart1 <- c(rep(0, length(yearf1)-trend), c(1:trend))
@@ -39,14 +39,16 @@ function(x, year=-1, trend=100, offset=TRUE, cl=0.95){
     newdata$yeart <- c(min(yeart):max(yeart))
   }
   
+  ann.vals <- ann.model.counts(x, year)$parms$parm
+  
   x.pred <- predict(x.lm, newdata, se.fit=TRUE)
-  x.pred$fit <- x.pred$fit - x.pred$fit[which(names(x.pred$fit)==(year-min(x$year)+1))]
-
+  x.pred$fit <- x.pred$fit + (mean(ann.vals) - mean(x.pred$fit))  
+  
   years <- c(min(x$year):max(x$year))
   res <- cbind(years, data.frame(cbind(parm=x.pred$fit,se=x.pred$se))) # necessary to stop factor conversion!
   res$index <- exp(res$parm)
-  res$annual <- ann.model.counts(x)$parms$index
-
+  res$annual <- exp(ann.vals)
+  
   cl.int <- qnorm(1-((1-cl)/2))
   res$lcl <- exp(res$parm - cl.int * res$se)
   res$ucl <- exp(res$parm + cl.int * res$se)
--- R/annt.model.prod.R
+++ R/annt.model.prod.R
@@ -28,9 +28,9 @@ function(x, year=-1, trend=100, offset=TRUE, cl=0.95){
     x$yearf <- ifelse ( x$year > ybreak, 0, x$year )
     x$yeart <- ifelse ( x$year > ybreak, x$year-ybreak, 0 )
     if( length(table(x$site)) > 1 )
-      x.lm <- glm(as.matrix(cbind(jvcaps,totcaps)) ~ as.factor(site) + as.factor(yearf) + yeart, family=quasibinomial, offset=offset, data=x)
+      x.lm <- glm(as.matrix(cbind(jvcaps,adcaps)) ~ as.factor(site) + as.factor(yearf) + yeart, family="quasibinomial", offset=offset, data=x)
     else
-      x.lm <- glm(as.matrix(cbind(jvcaps,totcaps)) ~ as.factor(yearf) + yeart, family=quasibinomial, offset=offset, data=x)
+      x.lm <- glm(as.matrix(cbind(jvcaps,adcaps)) ~ as.factor(yearf) + yeart, family="quasibinomial", offset=offset, data=x)
     
     yearf1 <- c(min(x$yearf[x$yearf>0]):max(x$yearf),rep(0,trend))
     yeart1 <- c(rep(0,length(yearf1)-trend),c(1:trend))
@@ -40,22 +40,24 @@ function(x, year=-1, trend=100, offset=TRUE, cl=0.95){
   else {
     x$yeart <- x$year # for compatibility with above
     if( length(table(x$site)) > 1 )
-      x.lm <- glm(as.matrix(cbind(jvcaps,totcaps)) ~ as.factor(site) + yeart, family=quasibinomial,offset=offset,data=x)
+      x.lm <- glm(as.matrix(cbind(jvcaps,adcaps)) ~ as.factor(site) + yeart, family=quasibinomial, offset=offset, data=x)
     else
-      x.lm <- glm(as.matrix(cbind(jvcaps,totcaps)) ~ yeart, family=quasibinomial,offset=offset,data=x)
+      x.lm <- glm(as.matrix(cbind(jvcaps,adaps)) ~ yeart, family=quasibinomial, offset=offset, data=x)
 
     newdata <- as.data.frame(cbind(site=rep(min(x$site),nyrs)))
     newdata$yeart <- c(min(x$yeart):max(x$yeart))
   }
 
+  ann.vals <- ann.model.prod(list(ad.data=cesdata$ad.data, jv.data=cesdata$jv.data), year)$parms$parm
+    
   x.pred <- predict(x.lm, newdata, se.fit=TRUE)
-  x.pred$fit <- x.pred$fit - x.pred$fit[which(names(x.pred$fit)==(year-min(x$year)+1))]
+  x.pred$fit <- x.pred$fit + (mean(ann.vals) - mean(x.pred$fit))  
 
   years <- c(min(x$year):max(x$year))
   res <- cbind(years, data.frame(cbind(parm=x.pred$fit,se=x.pred$se))) # necessary to stop factor conversion!
   res$index <- exp(res$parm)         # NOTE: log back-transform rather than logistic!! gives no jv per ad
-                                      #       rather simply ppn jvs
-  res$annual <- ann.model.prod(cesdata, year)$parms$index
+                                     #       rather simply ppn jvs
+  res$annual <- exp(ann.vals)
   
   cl.int <- qnorm(1-((1-cl)/2))
   res$lcl <- exp(res$parm - cl.int * res$se)

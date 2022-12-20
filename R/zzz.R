303ac4b32acd9864ec27240bc193160aa5a783ab
Rob Rob <rob.robinson@bto.org>
2022-12-20 16:06:23 +0000

    update to 1.0.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <-
 function(lib, pkg){
   
-  write("Welcome to cesr 0.51, use help(cesr) to get started", "")
+  write("Welcome to cesr 1.0.0, use help(cesr) to get started", "")
 
   utils::data(cesnames, package="cesr")
   

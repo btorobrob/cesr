27c8761f80f705aac075f6ffdcaa2260e3e2ae90
Rob Rob <rob.robinson@bto.org>
2023-05-03 16:27:10 +0100

    add arguments to give a bit more control

--- R/map.ces.R
+++ R/map.ces.R
@@ -1,13 +1,17 @@
 map.ces <-
 function(cesobj, type='c', xlim=c(-20,30), ylim=c(35,70), pch=21, col=c('white','red'), 
-         file=NULL, width=640, height=480, units='px', ...){
+         file=NULL, width=640, height=480, lwd=1, cex=1, units='px', mar=c(1,1,1,1), ...){
     
   # for pch: 21=circle, 22=square, 23=diamond, 24=up-triangle, 25=down-triangle
+  
+  oldpar <- par(list('pin', 'usr', 'mar'))
+  on.exit(par(oldpar))
   
   if( class(cesobj)[1]!='ces' )
        stop("No ces data\n")
   if( class(cesobj)[2]=='data' ){
     x <- cesobj[!duplicated(paste0(cesobj$countryID, "_", cesobj$sitename)), ]
+    x$current <- 1 # plot all sites same colour
   } else if( class(cesobj)[2]=='plots' ){
     x <- cesobj$sites
   } else if( class(cesobj)[2]=='sites' ){
@@ -15,7 +19,7 @@ function(cesobj, type='c', xlim=c(-20,30), ylim=c(35,70), pch=21, col=c('white',
   } else {
     stop("No map data to plot\n")
   }
-  x$current <- 1 # plot all sites same colour
+  #
   
   if( tolower(substr(type,1,1) == 'c') ) {  # Current vs non-current sites 
     col <- rep(col, length.out=2)
@@ -42,6 +46,8 @@ function(cesobj, type='c', xlim=c(-20,30), ylim=c(35,70), pch=21, col=c('white',
     if (length(col) < nlvl) 
       stop(sprintf("please supply at least %d colors", nlvl))
     x$col<-col[as.numeric(x$habitat)]  # Pick colors by habitat factor level
+  } else if( tolower(substr(type,1,1) == 's') ) { # Color by site
+    x$col <- col
   } else
     x$col <- col[1]
 
@@ -70,8 +76,9 @@ function(cesobj, type='c', xlim=c(-20,30), ylim=c(35,70), pch=21, col=c('white',
     }
   }
 
-  maps::map(xlim=xlim, ylim=ylim, mar=c(0, 0, 0, 0), ...)
-  points(x=x$long, y=x$lat, pch=pch, bg=x$col)
+  par(usr=c(xlim, ylim))
+  maps::map(xlim=xlim, ylim=ylim, mar=mar, ...)
+  points(x=x$long, y=x$lat, pch=pch, bg=x$col, cex=cex, lwd=lwd)
 
   if( ftype != 'stdio' )
     dev.off()

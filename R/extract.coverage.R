28b94237de3f63a11e0a455920402994ae4d578c
Rob Rob <rob.robinson@bto.org>
2023-05-13 17:57:42 +0100

    bugfix

--- R/extract.coverage.R
+++ R/extract.coverage.R
@@ -121,12 +121,12 @@ function(cesdata, early=NA,late=NA, min.visits=1, all.visits=0, exclude=list(yea
                   nrow(visit.cov[nbirds>0]), 'visits, with',
                   nrow(miss.vis), 'visits missing\n')
   cat(sumtxt)
-  if( nrow(miss.vis) > nrow(visit.cov[nbirds>0])/10 ){
-    ppn.miss <- floor(100*nrow(miss.vis)/nrow(visit.cov[nbirds>0]))
-    wmsg <- paste(ppn.miss, 'visits missing, this is more than 10% - has all.visits been specified correctly?')
-    warning(wmsg, call.=FALSE, immediate. = TRUE)
-  }
 
+  if( nrow(miss.vis) > nrow(visit.cov[nbirds>0])/10 ){
+    wmsg <- paste('more than 10% of visits are missing - has all.visits been specified correctly?')
+    warning(wmsg, call.=FALSE, immediate.=TRUE)
+  }
+  
   cesdata <- as.data.frame(cesdata) # convert back to df to use extract.sites
   class(cesdata) <- c('ces', 'data', 'data.frame')
   sites <- extract.sites(cesdata, exclude$sites) 

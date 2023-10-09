5b7ca5656ec07746b3ad60697bad02d00c62187e
Rob Rob <rob.robinson@bto.org>
2023-10-09 11:50:47 +0100

    print survival objects

--- R/print.ces.R
+++ R/print.ces.R
@@ -6,6 +6,7 @@ function(x, ...){
   else 
     switch( class(x)[2],
             res.table = summary.ces.table(x),
+            markfit = print(x$model$results$real),
             print(as.data.frame(x)))
 
 }
